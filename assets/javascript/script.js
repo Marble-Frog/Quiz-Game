e42d1a4861d16e24fa43b769a7802642aeba68c2
Marble-Frog <kadesamboateng@googlemail.com>
2024-04-05 19:57:18 +0100

    Added and tested code to run game and display questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,32 @@
-console.log("Heyo")
+document.addEventListener("DOMContentLoaded", function() {
+    const nextButton = document.getElementById("next-btn");
+    const questionElement = document.getElementById("question")
+    let currentQuestionIndex = 0;
+    let score = 0;
+
+    function runGame() {
+        currentQuestionIndex = 0;
+        score = 0;
+        nextButton.innerHTML = "Next";
+        showQuestion();
+    }
+    function showQuestion(){
+        let currentQuestion = questions[currentQuestionIndex];
+        let questionNo = currentQuestionIndex + 1;
+        questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    }
+    function incrementScore() {
+
+    }
+
+    function incrementAttempts() {
+
+    }
+
+    const questions = [
+        {
+            question: "What is the largest land animal in the world?",
+        }
+    ];
+    runGame();
+});
